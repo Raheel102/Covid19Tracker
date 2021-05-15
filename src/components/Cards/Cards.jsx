82f3aab81c8663e9c265d4bf10af5b0566754002
Raheel102 <raheel102@gmail.com>
2021-05-14 21:35:02 -0500

    Added main Project Files

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core'
+import CountUp from 'react-countup'
+import cx from 'classnames'
+
+import styles from './Cards.module.css'
+
+const Cards = ( { data: { confirmed, recovered, deaths, lastUpdate } } ) => {
+
+    if(!confirmed) {
+        return 'Loading...';
+    }
+
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Confirmed Cases</Typography>
+                        <Typography variant='h5'>
+                            <CountUp 
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography colr='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'>Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+                        <Typography variant='h5'>
+                            <CountUp 
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography colr='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'>Number of cured people of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+                        <Typography variant='h5'>
+                            <CountUp 
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography colr='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'> Number of deaths from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        
+        </div>
+    )
+}
+
+export default Cards
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api'
+import { Line, Bar } from 'react-chartjs-2'
+
+import styles from './Chart.module.css'
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+    const [dailyData, setDailyData] = useState([])
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData())
+        }
+
+        fetchAPI()
+    }, [])
+
+
+    const lineChart = (
+      dailyData.length !== 0
+        ? (
+            <Line 
+              data={{
+              labels: dailyData.map(({ date }) => date ),
+              datasets: [{
+                data: dailyData.map(({ confirmed }) => confirmed ),
+                  label: 'Confirmed Cases',
+                borderColor: '#3333ff',
+                fill: true
+              },
+              {
+                data: dailyData.map(({ deaths }) => deaths ),
+                  label: 'Deaths',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true
+              }],
+              }}
+            />
+        ) : null
+        
+    )
+
+    const barChart = (
+      confirmed
+        ? (
+          <Bar 
+            data={{
+              labels: ['Confirmed Cases', 'Recoveries', 'Deaths'],
+              datasets: [{
+                label: 'Number',
+                backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 9, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+                data:[confirmed.value, recovered.value, deaths.value]
+              }],
+            }}
+            options={{
+              legend: { dispay: false },
+              title: { display: true, text: `Actual Cases - ${country}` }
+            }}
+          />
+        ) : null
+    )
+
+    return (
+        <div className={styles.container}>
+          {country ? barChart : lineChart}
+        </div>
+    )
+}
+
+export default Chart
